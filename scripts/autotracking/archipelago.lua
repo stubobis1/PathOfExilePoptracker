89527dccebba3cc756bdb19af63f40dd4e7a55b4
stubob <mr.stubob@gmail.com>
2025-08-11 13:20:10 -0600

    fixed progressive items not working

--- scripts/autotracking/archipelago.lua
+++ scripts/autotracking/archipelago.lua
@@ -153,7 +153,7 @@ function onItem(index, item_id, item_name, player_number)
                 item_obj.Active = true
             elseif item_obj.Type == "progressive" then
                 -- print("progressive")
-                item_obj.Active = true
+                item_obj.CurrentStage = item_obj.CurrentStage + 1
             elseif item_obj.Type == "consumable" then
                 -- print("consumable")
                 item_obj.AcquiredCount = item_obj.AcquiredCount + item_obj.Increment * (tonumber(item_pair[3]) or 1)
